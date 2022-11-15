0ade9ebe6c9fd0ebf788d53ea1977441f7d850da
junhao-h <44504072+junhao-h@users.noreply.github.com>
2022-11-15 17:25:08 +0800

    send email feedback

--- src/components/HireMeModal.js
+++ src/components/HireMeModal.js
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { FiX } from "react-icons/fi";
 import Button from "./reusable/Button";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 const selectOptions = [
@@ -13,11 +13,13 @@ const selectOptions = [
 
 const HireMeModal = ({ onClose, onRequest }) => {
   const form = useRef();
+  const [loading, setLoading] = useState(false);
 
-  const sendEmail = (e) => {
+  const sendEmail = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
-    emailjs
+    await emailjs
       .sendForm(
         process.env.REACT_APP_SERVICE_ID,
         process.env.REACT_APP_TEMPLATE_ID,
@@ -33,6 +35,8 @@ const HireMeModal = ({ onClose, onRequest }) => {
           console.log(error.text);
         }
       );
+    setLoading(false);
+    onClose();
   };
 
   return (
@@ -132,19 +136,40 @@ const HireMeModal = ({ onClose, onRequest }) => {
                 <div className="mt-6 pb-4 sm:pb-1">
                   <span
                     type="submit"
-                    className="px-4
-											sm:px-6
+                    className="px-3
+											sm:px-3
 											py-2
 											sm:py-2.5
 											text-white
 											bg-indigo-500
 											hover:bg-indigo-600
 											rounded-md
-											focus:ring-1 focus:ring-indigo-900 duration-500"
+											focus:ring-1 focus:ring-indigo-900 duration-500 flex w-40 items-center"
                     aria-label="Submit Request"
                     value="Send"
                   >
-                    <Button title="Send Request" />
+                    {loading ? (
+                      <svg
+                        class="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-red-500"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="currentColor"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentFill"
+                        />
+                      </svg>
+                    ) : (
+                      <div className="mr-2 w-4 h-4"></div>
+                    )}
+                    <button disabled={loading} className="text-sm">
+                      Send Request
+                    </button>
                   </span>
                 </div>
               </form>
